f851bb7d6003bb77ba67fca7c0e0069baf6252a7
Shaon Dey <shaon.dey@edison-bd.com>
2018-07-11 18:14:33 +0600

    py dj 17 Added Uploading Media

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -1,14 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-#from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    #path('about/$', views.about),
-    #path('^$', views.homepage)
-
     url(r'^admin/', admin.site.urls),
     url(r'^articles/', include('articles.urls')),
     url(r'^about/$', views.about),
@@ -16,3 +13,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
