04bcb0a8b70ac6e13e6db3bdaa8319d3b6c238e9
Shaon Dey <shaon.dey@edison-bd.com>
2018-07-12 09:31:28 +0600

    py dj 18: Added Accounts App

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^articles/', include('articles.urls')),
     url(r'^about/$', views.about),
     url(r'^$', views.homepage),
