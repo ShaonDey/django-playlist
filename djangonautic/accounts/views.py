0e88925dc0cdcd6bd46e6447ac41647b0098fdbe
Shaon Dey <myselfshaon@gmail.com>
2018-07-12 16:04:31 +0600

    Removed a . from comment

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login
 
-# Create your views here.
+# Create your views here
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
